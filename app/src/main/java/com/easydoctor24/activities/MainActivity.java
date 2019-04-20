d21c3b20e9daf4faa30840076703d1c6eaf0651f
apurba <apurba10024@gmail.com>
2019-04-21 04:37:39 +0600

    1.2 Added initial package directory

--- app/src/main/java/com/easydoctor24/activities/MainActivity.java
+++ app/src/main/java/com/easydoctor24/activities/MainActivity.java
@@ -1,13 +1,15 @@
-package com.easydoctor24;
+package com.easydoctor24.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.easydoctor24.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
     }
 }
