1922dd6e305f7afc4a340bf8996ff71a3be0865f
apurba <apurba10024@gmail.com>
2019-04-21 17:04:11 +0600

    1.3 Added Paitent and Doctor

--- app/src/main/java/com/easydoctor24/activities/MainActivity.java
+++ app/src/main/java/com/easydoctor24/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.easydoctor24.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.easydoctor24.R;
 
@@ -11,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void patientClicked(View view) {
+        Toast.makeText(this, "Patient Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void doctorOnclick(View view) {
+        Toast.makeText(this, "Doctor Clicked", Toast.LENGTH_SHORT).show();
     }
 }
