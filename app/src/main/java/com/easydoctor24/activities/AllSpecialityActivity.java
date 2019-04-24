2819105bc18a6ff5f52cf9deb7f45e63cb0d8208
apurba <apurba10024@gmail.com>
2019-04-24 13:29:01 +0600

    1.9 Changed Launcher Activity from RegisterActivity to AllSpecialityActivity

---
+++ app/src/main/java/com/easydoctor24/activities/AllSpecialityActivity.java
@@ -0,0 +1,15 @@
+package com.easydoctor24.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.easydoctor24.R;
+
+public class AllSpecialityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_speciality);
+    }
+}
