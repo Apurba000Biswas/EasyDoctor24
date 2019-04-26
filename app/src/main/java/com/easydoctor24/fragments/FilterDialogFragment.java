bcc5c3920c70a147300e53836e1d8b91c8e90b3d
apurba <apurba10024@gmail.com>
2019-04-27 04:04:01 +0600

    3.9 Added Custom Seek Bar

--- app/src/main/java/com/easydoctor24/fragments/FilterDialogFragment.java
+++ app/src/main/java/com/easydoctor24/fragments/FilterDialogFragment.java
@@ -10,6 +10,8 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.easydoctor24.R;
 
@@ -34,7 +36,7 @@ public class FilterDialogFragment extends DialogFragment {
         String title = getArguments().getString("title");
         LayoutInflater factory = LayoutInflater.from(getContext());
         View rootView = factory.inflate(R.layout.fragment_filter_dialog, null);
-
+        setDialogView(rootView);
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
         alertDialogBuilder.setView(rootView);
@@ -44,6 +46,28 @@ public class FilterDialogFragment extends DialogFragment {
         Dialog dialog = alertDialogBuilder.create();
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         return dialog;
+    }
+
+    private void setDialogView(View rootView){
+        SeekBar filterBar = rootView.findViewById(R.id.sb_filter_bar);
+        filterBar.setMax(1000);
+        filterBar.setProgress(10);
+
+        filterBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getContext(), "Seek bar progress is :" + progressChangedValue,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
