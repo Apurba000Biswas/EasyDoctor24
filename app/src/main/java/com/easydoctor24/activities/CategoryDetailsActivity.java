6f04cc947c7512279fd97868a88b9afea0cddcd1
apurba <apurba10024@gmail.com>
2019-04-29 05:04:27 +0600

    4.3 Updated DoctorListItem Design

--- app/src/main/java/com/easydoctor24/activities/CategoryDetailsActivity.java
+++ app/src/main/java/com/easydoctor24/activities/CategoryDetailsActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.easydoctor24.R;
 import com.easydoctor24.adapters.DoctorFragmentPagerAdapter;
